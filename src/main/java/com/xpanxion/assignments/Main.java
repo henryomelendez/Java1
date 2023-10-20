ef382fe173bd0351ccaa66e183340c3c51b6ca98
Melendez <henryomar.melendez@fiserv.com>
2023-10-20 16:17:26 -0400

    Java 2 ex2 finish

--- src/main/java/com/xpanxion/assignments/Main.java
+++ src/main/java/com/xpanxion/assignments/Main.java
@@ -1,11 +1,12 @@
 package com.xpanxion.assignments;
 
 import com.xpanxion.assignments.student.JavaOne;
+import com.xpanxion.assignments.student.JavaTwo;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         JavaOne javaOne = new JavaOne();
-        javaOne.ex1();
+ //       javaOne.ex1();
 //        javaOne.ex2();
 //        javaOne.ex3();
 //        javaOne.ex4();
@@ -15,5 +16,8 @@ public class Main {
 //        javaOne.ex8();
 //        javaOne.ex9();
 //        javaOne.ex10();
+        JavaTwo javaTwo = new JavaTwo();
+ //       javaTwo.ex1();
+        javaTwo.ex2();
     }
 }
