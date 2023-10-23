8a76793f451a344337496a769a60af75be8c1214
Melendez <henryomar.melendez@fiserv.com>
2023-10-23 10:58:56 -0400

    added cats file

---
+++ src/main/java/com/xpanxion/assignments/student/Cat.java
@@ -0,0 +1,19 @@
+package com.xpanxion.assignments.student;
+
+public class Cat {
+    private String name;
+    public Cat(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
